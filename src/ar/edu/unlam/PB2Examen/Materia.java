c1bdc6a4c8d6a3a51fd9bafac2cb32fbfc3b56c4
Emanuel Cisterna <85168333+emanuelcodigos@users.noreply.github.com>
2021-10-06 17:26:51 -0300

    Agos (#5)
    * agrego clase Materia
    
    * corregi error en Materia
    
    * agrego clase Materia
    
    Co-authored-by: Agostina <aherdeiro@alumno.unlam.edu.ar>

---
+++ src/ar/edu/unlam/PB2Examen/Materia.java
@@ -0,0 +1,50 @@
+package ar.edu.unlam.PB2Examen;
+
+public class Materia {
+
+	private NombreMateria nombreMateria;
+	private String descripcion;
+	private Integer codigoMateria;
+	private String horario;
+
+	public Materia(NombreMateria nombreMateria, String descripcion, Integer codigoMateria, String horario) {
+		super();
+		this.nombreMateria = nombreMateria;
+		this.descripcion = descripcion;
+		this.codigoMateria = codigoMateria;
+		this.horario = horario;
+	}
+
+	public NombreMateria getTitulo() {
+		return nombreMateria;
+	}
+
+	public void setTitulo(NombreMateria nombreMateria) {
+		this.nombreMateria = nombreMateria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getCodigoMateria() {
+		return codigoMateria;
+	}
+
+	public void setCodigoMateria(Integer codigoMateria) {
+		this.codigoMateria = codigoMateria;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+}
