f0e9596862eaf026885775950a31a61b40978205
Emanuel Cisterna <85168333+emanuelcodigos@users.noreply.github.com>
2021-10-06 17:24:23 -0300

    Santiago livieri (#4)
    * Curso
    
    * Atributos Basicos de Curso
    
    Co-authored-by: SantiagoLivieri <91610865+LivieriSantiago@users.noreply.github.com>

---
+++ src/ar/edu/unlam/PB2Examen/Curso.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.PB2Examen;
+
+public class Curso {
+ //hola
+	
+	private Integer cantidadMinimaDeAlumnos;
+	private Integer cantidadMaximaDeAlumnos; 
+	private String fechaIninicalCursada;
+	private String fechaFinalCursada;
+	//private materia;
+}
